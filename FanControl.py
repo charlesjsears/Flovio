04978a4051e397821cc31925fa39f473cc6636bc
TomHebe <80727382+TomHebe@users.noreply.github.com>
2021-05-27 19:53:25 -0500

    Update FanControl.py

--- FanControl.py
+++ FanControl.py
@@ -10,19 +10,19 @@ p.ChangeDutyCycle(0)
 while(True):
         x = input('type l for low, m for medium and h for high:')
         if(x == 'l'):
-            p.ChangeDutyCycle(50)
-            break
+                p.ChangeDutyCycle(50)
+            
         elif(x == 'm'):
-            p.ChangeDutyCycle((75)
-            break
+                p.ChangeDutyCycle((75)
+            
         elif (x == 'h'):
-            p.ChangeDutyCycle(100)
-            break
+                p.ChangeDutyCycle(100)
+           
         elif (x == 's'):
                 p.ChangeDutyCycle(0)
-                break
+            
         else:
-            print('please enter either 1, m, s and h')
+                print('Please enter either l, m, h or s')
 
 #input('Press return to stop:')
 #p.stop()
