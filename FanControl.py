2cf0d0f137c0e0d9d9eee8f241362b5328c42310
TomHebe <80727382+TomHebe@users.noreply.github.com>
2021-05-27 20:22:48 -0500

    Update FanControl.py

--- FanControl.py
+++ FanControl.py
@@ -1,11 +1,11 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(32, GPIO.OUT)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(13, GPIO.OUT)
 
-p = GPIO.PWM(32, 5)
-p.ChangeDutyCycle(0)
+p = GPIO.PWM(13, 100)
+p.start(0)
 
 while(True):
         x = input('type l for low, m for medium and h for high:')
@@ -17,10 +17,11 @@ while(True):
             p.ChangeDutyCycle(100)
         elif (x == 's'):
             p.ChangeDutyCycle(0)
+            break
             
         else:
             print('Please enter either l, m, h or s')
 
 #input('Press return to stop:')
-#p.stop()
+p.stop()
 GPIO.cleanup()
